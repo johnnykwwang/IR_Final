4f045d86254d81cc883efc61d333482c075279a8
Johnny@Ash <johnny.kwwang@gmail.com>
2017-06-06 17:10:00 +0800

    update crawler

--- ClassCentralCrawler.py
+++ ClassCentralCrawler.py
@@ -1,8 +1,11 @@
 from pymongo import MongoClient
+from IPython import embed
+from bs4 import BeautifulSoup
+import requests
 
 
 class ClassCentralCrawler:
-    DB_URL = 'mongodb://ir_final:irfinal2017@ds155091.mlab.com:55091/ir_final'
+    DB_URI = 'mongodb://ir_final:irfinal2017@ds155091.mlab.com:55091/ir_final'
     db = None
     collection = None
 
@@ -14,9 +17,13 @@ class ClassCentralCrawler:
     def parse(self,URL):
         response = requests.get(URL).text
         soup = BeautifulSoup(response,'html.parser')
+        embed()
         course_obj = {  'title': soup.h1.text, 
                         'description': soup.find_all("div",class_="course-desc")[0],
                         'link': soup.find_all("a", class_="cta-button btn_go_to_class")[0]['href'],
                         'taglist':  [ x.text.rstrip() for x in soup.find_all("div", class_="taglist")[0].find_all('a') ],
                         }
+        self.collection.insert_one(course_obj)
 
+crawler = ClassCentralCrawler()
+crawler.parse("https://www.class-central.com/mooc/2161/coursera-learning-how-to-learn-powerful-mental-tools-to-help-you-master-tough-subjects")
