f39408ba0fccd143b593cbaaa4fa5ecb30b8e013
b02501091 <b02501091@linux13.csie.ntu.edu.tw>
2017-06-18 17:45:36 +0800

    connect to abbrevations.com

---
+++ abbreviation.py
@@ -0,0 +1,73 @@
+import sys
+from xml.etree import ElementTree as ET
+
+import requests
+
+
+class Abbreviate:
+
+    abbreviation_url = [
+        'http://www.stands4.com/services/v2/abbr.php',
+    ]
+    # uid
+    # tid
+
+    def __init__(self):
+        pass
+
+    def accept_query_term(self):
+        term = input("Enter the query term: ")
+        self.query_term = term
+
+    def de_abbreviation(self, term=None, cate_id='all'):
+        if term is None:
+            term = self.query_term
+
+
+        payload = {
+            'sortby': 'p',
+            'term': term,
+            'categorid': cate_id,
+            'uid': self.uid,
+            'tokenid': self.tid,
+            'searchtype': 'e'
+        }
+        
+        response = requests.get(self.urls[0], params=payload)
+        return response
+
+    def parse_abb_response(self, response, cate=None):
+        # parse the response from www.abbreivations.com
+        root = ET.fromstring(response)
+        if cate is not None:
+            result = [root[i][1].text for i in range(len(root)) if root[i][2] == cate]
+        else:
+            result = [root[i][1].text for i in range(len(root))]
+        return result
+        
+    def run_deabbreviate(self, term=None):
+        if term is None:
+            self.accept_query_term()
+        else:
+            self.query_term = term
+
+        response = self.de_abbreviation()
+        results = self.parse_abb_response(response)
+        return results
+
+
+def main():
+    try:
+        term = sys.argv[1]
+    except Exception:
+        term = None
+
+    a = Abbreviate()
+    results = a.run_deabbreviate(term)
+    print(results)
+    return results
+
+
+if __name__ == '__main__':
+    main()
+
