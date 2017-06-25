1cca993e0d115f03ad6ace4571bca2c05bdff16e
Yi Hsiao <hsiaoyi0504@gmail.com>
2017-06-25 15:52:53 +0800

    modified to only one keyword for each querying

--- lookup.py
+++ lookup.py
@@ -24,10 +24,8 @@ class Lookup:
         stemmer = PorterStemmer()
         stems = self.stem_tokens(tokens, stemmer)
         return stems
-        # return tokens
 
     def retrieve(self, keywords=["Algorithms"], load_pickle=True):
-        # clf = None
         target_dir = './mit_course_subtitles'
         coursera_train = load_files(target_dir,load_content=True)
         if load_pickle:
@@ -38,20 +36,19 @@ class Lookup:
             X_train_counts = count_vect.fit_transform(coursera_train.data)
             tfidf_transformer = TfidfTransformer()
             X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
-            # print(count_vect.get_feature_names())
             clf = SGDClassifier(loss='log', penalty='l2',alpha=1e-3, n_iter=5, random_state=42).fit(X_train_tfidf, coursera_train.target)
             with open('classifier-pickle','wb') as f:
                 pickle.dump([clf, count_vect, tfidf_transformer],f)
-        crawler = WikiCrawler()
+        # crawler = WikiCrawler()
         # defnition, words = crawler.get_definition(keywords)
-        X_new_counts = count_vect.transform(keywords)
+        X_new_counts = count_vect.transform([keywords])
         X_new_tfidf = tfidf_transformer.transform(X_new_counts)
         predicted = clf.predict(X_new_tfidf)
         probs = clf.predict_proba(X_new_tfidf)
         n = 5
         best_n = np.argsort(probs, axis=1)[:,::-1][:,:n]
-        for doc, categories in zip(keywords, best_n):
-            print("{}=>".format(doc))
+        print("{}=>".format(keywords))
+        for categories in best_n:
             for category in categories:
                 print(coursera_train.target_names[category])
             print('\n', end='')
@@ -60,4 +57,5 @@ if __name__ == '__main__':
     lkp = Lookup()
     # keywords = ["Data Structure"]
     keywords = ['algorithm and data structure', 'computer network','minimum spanning tree']
-    lkp.retrieve(keywords=keywords, load_pickle=True)
+    for key in keywords:
+        lkp.retrieve(keywords=key, load_pickle=True)
